76adbc7578a20c900e43757214228e8b1e3f8ac1
thenandiniar <thenandiniar@gmail.com>
2026-06-14 06:26:57 +0530

    Fixed Render deployment

--- app.py
+++ app.py
@@ -156,6 +156,5 @@ def index():
 
 
 if __name__ == "__main__":
-    # Start in debug mode as requested
-    app.run(debug=True)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
